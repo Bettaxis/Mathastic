79ca0ef381fa4399af515dc555eb1c37264b0ced
Russ <russellsng@gmail.com>
2018-02-02 23:27:05 -0500

    Initial commit, tweaked .GitIgnore for extra unity stuff, feel free to add more.
    Basic level concept and camera set-up, and script for spawning number blocks(space bar to spawn).

---
+++ Mathastic/Assets/Scripts/SpawnCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCube : MonoBehaviour {
+
+    public GameObject block;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	if (Input.GetKeyDown(KeyCode.Space)) // Spawns on Spacebar
+        {
+           GameObject copy;
+            copy = Instantiate(block, transform.position, transform.rotation);
+            copy.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.down);
+        }
+	}
+}
